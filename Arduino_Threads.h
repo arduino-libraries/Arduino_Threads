c3bdd06a77d0f38e91e3f93e1f918ceeee514014
Dario Pennisi <dario@iptronix.com>
2020-09-01 20:05:56 +0200

    added subclassing and methods for thread synchronization

--- Arduino_Threads.h
+++ Arduino_Threads.h
@@ -33,78 +33,47 @@ class Shared // template definition
 #define INCF(F) INCF_(F)
 #define INCF_(F) #F
 
-class ArduinoThreadClass {
-public:
-  void add(rtos::Thread* me) {
-    if (controller == NULL) {
-        controller = new rtos::Thread(osPriorityHigh, 512);
-        controller->start(mbed::callback(this, &ArduinoThreadClass::loop));
-    }
-    list[idx++] = me;
-  }
-
-  void ping(rtos::Thread* me) {
-    for (int i=0; i < idx; i++) {
-      if (me == list[i]) {
-        timestamps[i] = millis();
-      }
-    }
-  }
-
-private:
-
-  void loop() {
-    while (1) {
-      for (int i = 0; i < idx; i++) {
-        if (millis() - timestamps[i] > 5000) {
-          // list[i]->terminate();
-          // TODO: reorder threads and make list[i] == NULL
-          // Serial.println(list[i]->get_name() + String(" killed "));
-        }
-      }
-      delay(1000);
-    }
-  }
-
-  rtos::Thread* controller = NULL;
-  rtos::Thread* list[10] = {NULL};
-  uint32_t timestamps[10] = {0};
-  int idx = 0;
-};
-
-ArduinoThreadClass ArduinoThread;
-
 #define _macroToString(sequence) #sequence
 
-#define THD_ENTER(tabname) class CONCAT(tabname, Class) { \
 
-#define THD_DONE(tabname)   private: \
-    void execute() {                 \
-      ArduinoThread.add(t);          \
-      setup();                       \
-      while (1) {                    \
-        loop();                      \
-        ArduinoThread.ping(t);       \
-      }                              \
-    }                                \
-    rtos::Thread* t;                 \
-                                     \
-  public:                            \
-    void start(int stacksize = 4096) {            \
-      t = new rtos::Thread(osPriorityNormal, stacksize, nullptr, _macroToString(tabname)); \
-      t->start(mbed::callback(this, &CONCAT(tabname,Class)::execute));  \
-    }                                \
-    void begin() {                   \
-      start();                       \
-    }                                \
-};                                   \
+class ArduinoThreads {
+  private:
+    rtos::EventFlags *GlobalEvents;
+    rtos::EventFlags ThreadEvents;
+    virtual void setup(void) {};
+    virtual void loop(void) {};
+    void execute() {                 
+      setup();
+      if (GlobalEvents!=NULL) GlobalEvents->wait_all(LoopStart);                       
+      while ( (GlobalEvents==NULL || !(GlobalEvents->get()&LoopStop) ) &&
+             !(ThreadEvents.get()&LoopStop) ) {                    
+        loop();                      
+      }                              
+    }                                
+    rtos::Thread t;                  
+  public:
+    typedef enum {
+      LoopStart= (1<<0),
+      LoopStop=  (1<<1)
+    } Events;
+    void start(rtos::EventFlags *event = NULL) {
+      GlobalEvents = event;
+      t.start(mbed::callback(this, &ArduinoThreads::execute));  
+    } 
+    void terminate() {
+      t.terminate();
+    }
+    void stop() {
+      ThreadEvents.set(LoopStop);
+    }
+};
+
+#define THD_ENTER(tabname) class CONCAT(tabname, Class) : public ArduinoThreads { \
+private:
+
+#define THD_DONE(tabname) \
+};  \
 CONCAT(tabname,Class) tabname;
-
-/*
-#define NEWTHREAD(tabname) THD_ENTER(tabname) \
-                           \#include CONCAT(tabname, .h) \
-                           THD_DONE(tabname)
-*/
 
 #include "Wire.h"
 #include "SerialDispatcher.h"
