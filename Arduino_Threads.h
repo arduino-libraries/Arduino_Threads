bf3034e2fafddcc24149e4b119c85822c98c78da
Dario Pennisi <dario@iptronix.com>
2020-09-03 19:08:59 +0200

    restored stack size and added better synchronization primitives

--- Arduino_Threads.h
+++ Arduino_Threads.h
@@ -38,35 +38,70 @@ class Shared // template definition
 
 class ArduinoThreads {
   private:
-    rtos::EventFlags *GlobalEvents;
-    rtos::EventFlags ThreadEvents;
+    static rtos::EventFlags globalEvents;
+    uint32_t startFlags;
+    uint32_t stopFlags;
+    uint32_t loopDelay;
     virtual void setup(void) {};
     virtual void loop(void) {};
-    void execute() {                 
+    void execute() {
       setup();
-      if (GlobalEvents!=NULL) GlobalEvents->wait_all(LoopStart);                       
-      while ( (GlobalEvents==NULL || !(GlobalEvents->get()&LoopStop) ) &&
-             !(ThreadEvents.get()&LoopStop) ) {                    
-        loop();                      
-      }                              
-    }                                
-    rtos::Thread t;                  
-  public:
-    typedef enum {
-      LoopStart= (1<<0),
-      LoopStop=  (1<<1)
-    } Events;
-    void start(rtos::EventFlags *event = NULL) {
-      GlobalEvents = event;
-      t.start(mbed::callback(this, &ArduinoThreads::execute));  
-    } 
-    void terminate() {
-      t.terminate();
+      // if startFlags have been passed then wait until all the flags are set
+      // before starting the loop. this is used to synchronize loops from multiple
+      // sketches.
+      globalEvents.wait_all(startFlags);      
+
+      // if stopFlags have been passed stop when all the flags are set
+      // otherwise loop forever
+      while ( 1 ) {
+        loop();
+        // on exit clear the flags that have forced us to stop.
+        // note that if two groups of sketches stop on common flags
+        // the first group will clear them so the second group may never
+        // exit
+        if (stopFlags!=0) {
+          if ((globalEvents.get()&stopFlags)!=stopFlags) {
+            globalEvents.clear(stopFlags);
+            return;
+          }
+          if ((rtos::ThisThread::flags_get()&stopFlags)!=stopFlags) {
+            rtos::ThisThread::flags_clear(stopFlags);
+            return;
+          }
+        }
+        // sleep for the time we've been asked to insert between loops 
+        rtos::ThisThread::sleep_for(loopDelay);
+      }
     }
-    void stop() {
-      ThreadEvents.set(LoopStop);
+    rtos::Thread *t;
+  public:
+    // start this sketch
+    void start(int stacksize = 4096, uint32_t startFlags=0, uint32_t stopFlags=0) {
+      this->startFlags = startFlags;
+      this->stopFlags = stopFlags;
+      loopDelay=0;
+      t = new rtos::Thread(osPriorityNormal, stacksize, nullptr, _macroToString(tabname)); 
+      t->start(mbed::callback(this, &ArduinoThreads::execute));  
+    } 
+    // kill this sketch
+    void terminate() {
+      t->terminate();
+    }
+    // send an event to all sketches at the same time
+    static void broadcastEvent(uint32_t event) {
+      globalEvents.set(event);
+    }
+    // send an event only to this sketch
+    void sendEvent(uint32_t event) {
+      t->flags_set(event);
+    }
+    // set the rate at which loop function will be called
+    void setLoopDelay(uint32_t delay) {
+      loopDelay = delay;
     }
 };
+
+rtos::EventFlags ArduinoThreads::globalEvents;
 
 #define THD_ENTER(tabname) class CONCAT(tabname, Class) : public ArduinoThreads { \
 private:
