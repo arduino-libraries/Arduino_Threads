e58455326a88d17bae3cd454471e1804fedde48e
Alexander Entinger <cto@lxrobotics.com>
2021-10-11 08:41:02 +0200

    Replace rtos::Queue/MemoryPool with rtos::Mail which contains both in one class.

--- src/Shared.hpp
+++ src/Shared.hpp
@@ -13,50 +13,59 @@
  **************************************************************************************/
 
 template<class T, size_t QUEUE_SIZE = 16>
-class Shared // template definition
+class Shared
 {
   public:
     Shared() {
     }
-    operator T() {
-      osEvent evt = queue.get();
-      if (evt.status == osEventMessage) {
-        /* Obtain the oldest inserted element from the queue. */
-        T * val_ptr = reinterpret_cast<T *>(evt.value.p);
+
+    operator T()
+    {
+      T * val_ptr = _mailbox.try_get_for(rtos::Kernel::wait_for_u32_forever);
+      if (val_ptr)
+      {
         /* Copy the content of T stored in the memory pool since we'll have to free the memory pool afterwards. */
         T const tmp_val = *val_ptr;
         /* Free the allocated memory in the memory pool. */
-        memory_pool.free(val_ptr);
-        /* Return obtained value from queue. */
+        _mailbox.free(val_ptr);
+        /* Return obtained value from _mailbox. */
         return tmp_val;
       }
       return val;
     }
-    T& operator= (const T& other) {
-      if (queue.full()) {
-        // invokes operator T() to discard oldest element and free its memory
-        T discard = *this;
-      }
+
+    void operator = (T const & other)
+    {
+      if (_mailbox.full())
+        T discard = *this; /* Invokes operator T() to discard oldest element and free its memory. */
+
       val = other;
-      /* Allocate memory in the memory pool. */
-      T * val_ptr = memory_pool.alloc();
-      /* Copy the content of 'other' into the freshly allocated message. */
-      *val_ptr = other;
-      /* Insert into queue. */
-      queue.put(val_ptr);
-      return (*val_ptr);
+
+      /* Try and allocate memory for the new entry in rtos::Mail. */
+      T * val_ptr = _mailbox.try_alloc();
+      if (val_ptr)
+      {
+        /* Copy the content of 'other' into the freshly allocated message. */
+        *val_ptr = other;
+        /* Copy the content of 'other' into the peek value. */
+        val = other;
+        /* Insert value into mailbox. */
+        _mailbox.put(val_ptr);
+      }
     }
+
     T& peek() {
       return val;
     }
+
     T& latest() {
       return peek();
     }
+
   private:
 
     T val;
-    rtos::MemoryPool<T, QUEUE_SIZE> memory_pool;
-    rtos::Queue<T, QUEUE_SIZE> queue;
+    rtos::Mail<T, QUEUE_SIZE> _mailbox;
 };
 
 #endif /* ARDUINO_THREADS_SHARED_HPP_ */
