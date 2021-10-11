83a748d4bab32f87656b1ec102bc0733ddf2e954
Alexander Entinger <cto@lxrobotics.com>
2021-10-11 08:47:00 +0200

    Separate implementation and definition.

--- src/Shared.hpp
+++ src/Shared.hpp
@@ -19,40 +19,8 @@ class Shared
     Shared() {
     }
 
-    operator T()
-    {
-      T * val_ptr = _mailbox.try_get_for(rtos::Kernel::wait_for_u32_forever);
-      if (val_ptr)
-      {
-        /* Copy the content of T stored in the memory pool since we'll have to free the memory pool afterwards. */
-        T const tmp_val = *val_ptr;
-        /* Free the allocated memory in the memory pool. */
-        _mailbox.free(val_ptr);
-        /* Return obtained value from _mailbox. */
-        return tmp_val;
-      }
-      return val;
-    }
-
-    void operator = (T const & other)
-    {
-      if (_mailbox.full())
-        T discard = *this; /* Invokes operator T() to discard oldest element and free its memory. */
-
-      val = other;
-
-      /* Try and allocate memory for the new entry in rtos::Mail. */
-      T * val_ptr = _mailbox.try_alloc();
-      if (val_ptr)
-      {
-        /* Copy the content of 'other' into the freshly allocated message. */
-        *val_ptr = other;
-        /* Copy the content of 'other' into the peek value. */
-        val = other;
-        /* Insert value into mailbox. */
-        _mailbox.put(val_ptr);
-      }
-    }
+    operator T();
+    void operator = (T const & other);
 
     T& peek() {
       return val;
@@ -67,5 +35,38 @@ class Shared
     T val;
     rtos::Mail<T, QUEUE_SIZE> _mailbox;
 };
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+template<class T, size_t QUEUE_SIZE>
+Shared<T,QUEUE_SIZE>::operator T()
+{
+  T * val_ptr = _mailbox.try_get_for(rtos::Kernel::wait_for_u32_forever);
+  if (val_ptr)
+  {
+    T const tmp_val = *val_ptr;
+    _mailbox.free(val_ptr);
+    return tmp_val;
+  }
+  return val;
+}
+
+template<class T, size_t QUEUE_SIZE>
+void Shared<T,QUEUE_SIZE>::operator = (T const & other)
+{
+  if (_mailbox.full())
+    T discard = *this;
+
+  val = other;
+
+  T * val_ptr = _mailbox.try_alloc();
+  if (val_ptr)
+  {
+    *val_ptr = other;
+    _mailbox.put(val_ptr);
+  }
+}
 
 #endif /* ARDUINO_THREADS_SHARED_HPP_ */
