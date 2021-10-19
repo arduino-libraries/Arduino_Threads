70eaaf2ed55ab98f003ff35413e4d2eb1387f9ca
Alexander Entinger <cto@lxrobotics.com>
2021-10-19 08:36:12 +0200

    Use Arduino_Threads for Threadsafe_Serial_GlobalPrefixSuffix.

--- examples/Threadsafe_IO/Threadsafe_Serial_GlobalPrefixSuffix/Threadsafe_Serial_GlobalPrefixSuffix.ino
+++ examples/Threadsafe_IO/Threadsafe_Serial_GlobalPrefixSuffix/Threadsafe_Serial_GlobalPrefixSuffix.ino
@@ -3,32 +3,12 @@
  **************************************************************************************/
 
 #include <Arduino_Threads.h>
-
-/**************************************************************************************
- * CONSTANTS
- **************************************************************************************/
-
-static size_t constexpr NUM_THREADS = 5;
-
 /**************************************************************************************
  * FUNCTION DECLARATION
  **************************************************************************************/
 
 String serial_log_message_prefix(String const & /* msg */);
 String serial_log_message_suffix(String const & prefix, String const & msg);
-void   serial_thread_func();
-
-/**************************************************************************************
- * GLOBAL VARIABLES
- **************************************************************************************/
-
-static char thread_name[NUM_THREADS][32];
-#undef Serial
-#ifdef ARDUINO_PORTENTA_H7_M4
-  SerialDispatcher Serial(Serial1); /* No SerialUSB for Portenta H7 / M4 Core */
-#else
-  SerialDispatcher Serial(SerialUSB);
-#endif
 
 /**************************************************************************************
  * SETUP/LOOP
@@ -36,21 +16,28 @@ static char thread_name[NUM_THREADS][32];
 
 void setup()
 {
+  Serial.begin(9600);
+  while (!Serial) { }
+
   Serial.global_prefix(serial_log_message_prefix);
   Serial.global_suffix(serial_log_message_suffix);
 
-  /* Fire up some threads all accessing the LSM6DSOX */
-  for(size_t i = 0; i < NUM_THREADS; i++)
-  {
-    snprintf(thread_name[i], sizeof(thread_name[i]), "Thread #%02d", i);
-    rtos::Thread * t = new rtos::Thread(osPriorityNormal, OS_STACK_SIZE, nullptr, thread_name[i]);
-    t->start(serial_thread_func);
-  }
+  Thread_1.start();
+  Thread_2.start();
+  Thread_3.start();
 }
 
 void loop()
 {
+  Serial.block();
+  Serial.println("Thread #0: Lorem ipsum ...");
+  Serial.unblock();
 
+  /* If we don't hand back control then the main thread
+   * will hog the CPU and all other thread's won't get
+   * time to be executed.
+   */
+  rtos::ThisThread::yield();
 }
 
 /**************************************************************************************
@@ -67,21 +54,4 @@ String serial_log_message_prefix(String const & /* msg */)
 String serial_log_message_suffix(String const & prefix, String const & msg)
 {
   return String("\r\n");
-}
-
-void serial_thread_func()
-{
-  Serial.begin(9600);
-
-  for(;;)
-  {
-    /* Sleep between 5 and 500 ms */
-    rtos::ThisThread::sleep_for(rtos::Kernel::Clock::duration_u32(random(5,500)));
-    /* Print a unbroken log message including thread name and timestamp as a prefix. */
-    Serial.block();
-    Serial.print(rtos::ThisThread::get_name());
-    Serial.print(": ");
-    Serial.print("Lorem ipsum ...");
-    Serial.unblock();
-  }
 }
