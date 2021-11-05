8d5be3bb3336d61e38e306577ee411c21aaa1ada
Alexander Entinger <cto@lxrobotics.com>
2021-11-05 10:11:21 +0100

    Remove superflous rtos::ThisThread::yield() from main thread 'loop()' See #45.

--- examples/Threadsafe_IO/Threadsafe_Wire/Threadsafe_Wire.ino
+++ examples/Threadsafe_IO/Threadsafe_Wire/Threadsafe_Wire.ino
@@ -45,11 +45,7 @@ void setup()
 
 void loop()
 {
-  /* If we don't hand back control then the main thread
-   * will hog the CPU and all other thread's won't get
-   * time to be executed.
-   */
-  rtos::ThisThread::yield();
+
 }
 
 /**************************************************************************************
